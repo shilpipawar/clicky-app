f99adc8f5d0316fbe0c43409a3be2031101f0b29
shilpipawar <shilpipawar@gmail.com>
2019-02-11 00:42:33 -0600

    2nd checking skeleton

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import PictureCard from "./components/PictureCard";
+import Wrapper from "./components/Wrapper";
+import picture from "./pictures.json";
 
 class App extends Component {
+  // Setting this.state.picture to the picture json array
+  state = {
+    picture
+  };
+
+  removePicture = id => {
+    // Filter this.state.picture for picture with an id not equal to the id being removed
+    const picture = this.state.picture.filter(picure => picture.id !== id);
+    // Set this.state.picture equal to the new picture array
+    this.setState({ picture });
+  };
+
+  // Map over this.state.picture and render a pictureCard component for each picture object
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+        {this.state.picture.map(picture => (
+          <PictureCard
+            id={picture.id}
+            key={picture.id}
+            image={picture.image}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
